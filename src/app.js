862e742422055d444b3a852ab5f9df1f2b240d73
Helen0105 <marinenkoolena21@gmail.com>
2026-06-30 12:18:54 +0300

    final commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 
 
 
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('get-students-btn').addEventListener('click', getStudents);
   document.getElementById('add-student-form').addEventListener('submit', addStudent);
 
-  // Слухач кліків для кнопок "Оновити" та "Видалити" у таблиці
   document.querySelector("#students-table tbody").addEventListener("click", (e) => {
     const id = e.target.dataset.id;
 
@@ -17,21 +13,33 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     if (e.target.classList.contains("edit-btn")) {
-      updateStudent(id); 
+     
+      currentId = id; 
+      const currentRow = e.target.closest("tr");
+      const form = document.getElementById("add-student-form");
+      
+      form.elements[0].value = currentRow.children[1].textContent;
+      form.elements[1].value = currentRow.children[2].textContent;
+      form.elements[2].value = currentRow.children[3].textContent;
+      form.elements[3].value = currentRow.children[4].textContent;
+      form.elements[4].value = currentRow.children[5].textContent; 
+      form.elements[5].checked = currentRow.children[6].textContent.trim() === "Так";
     }
   });
 });
 
-// Функція для отримання всіх студентів
-function getStudents() {
-  fetch("http://localhost:3000/students")
-    .then(response => response.json())
-    .then(data => {
-      renderStudents(data);
-    })
-    .catch(error => console.log(error));
-}
+let currentId = null;
 
+// Функція для отримання всіх студентів 
+async function getStudents() {
+  try {
+    const response = await fetch("http://localhost:3000/students")
+    const data = await response.json()
+    renderStudents(data);
+  } catch(error) {
+    console.log(error);
+  }
+}
 
 function renderStudents(students) {
   const tbody = document.querySelector("#students-table tbody");
@@ -56,7 +64,7 @@ function renderStudents(students) {
 }
 
 // Функція для додавання нового студента
-function addStudent(e) {
+async function addStudent(e) {
   e.preventDefault(); 
   
   const nameValue = document.getElementById("name").value;
@@ -75,30 +83,45 @@ function addStudent(e) {
     isEnrolled: isEnrolledValue
   };
 
-  fetch("http://localhost:3000/students", {
-    method: "POST", 
-    headers: {
-      "Content-Type": "application/json" 
-    },
-    body: JSON.stringify(newStudent) 
-  })
-  .then(response => response.json())
-  .then(() => {
+  
+  if (currentId) {
+    try {
+      await updateStudent(currentId, newStudent);
+      getStudents();
+      document.getElementById('add-student-form').reset();
+      currentId = null;
+      return;
+    } catch (error) {
+      console.log("Помилка");
+      return;
+    }
+  }
+
+  try {
+    const response = await fetch("http://localhost:3000/students", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json" 
+      },
+      body: JSON.stringify(newStudent) 
+    });
+    await response.json();
     document.getElementById('add-student-form').reset();
     getStudents();
-  })
-  .catch(error => console.log(error));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// Функція для оновлення студента (PATCH)
-function updateStudent(id, data) {
+// Функція для оновлення студента
+async function updateStudent(id, data) {
   const options = {
-        method: "PUT",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8",
-        },
-    };
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  };
 
 //   fetch(`http://localhost:3000/students/${id}`, {
 //     method: "PATCH", 
@@ -113,31 +136,24 @@ function updateStudent(id, data) {
 //   .catch(error => console.log(error));
 // } 
 
+  try {
+    const res = await fetch(`http://localhost:3000/students/${id}`, options);
+    const response = await res.json();
+    return response;
+  } catch (error) {
+    console.error("помилка в updateStudent:", error);
+    throw error;
+  }
+} 
 
-return fetch(`http://localhost:3000/students/${id}`, options)
-        .then(res => {
-          
-        
-           
-            return res.json();
-        })
-        .catch(error => {
-            console.error("помилка в updateStudent:", error);
-            throw error;
-        });
-
-
-
-      }
-
-
-// Функція для видалення студента (DELETE)
-function deleteStudent(id) {
-  fetch(`http://localhost:3000/students/${id}`, {
-    method: "DELETE"
-  })
-  .then(() => {
+// Функція для видалення студента 
+async function deleteStudent(id) {
+  try {
+    await fetch(`http://localhost:3000/students/${id}`, {
+      method: "DELETE"
+    });
     getStudents(); 
-  })
-  .catch(error => console.log(error));
+  } catch (error) {
+    console.log(error);
+  }
 }
