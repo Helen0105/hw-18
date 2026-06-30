ebf82d8bffefe4fc3ed92eb8c8a02d81e4153db9
Helen0105 <marinenkoolena21@gmail.com>
2026-06-30 11:11:08 +0300

    10

--- src/app.js
+++ src/app.js
@@ -1,159 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     document.getElementById('get-students-btn').addEventListener('click', getStudents);
-//   document.getElementById('add-student-form').addEventListener('submit', addStudent);
-//   document.querySelector("#students-table tbody").addEventListener("click", (e) => {
-//     const id = e.target.dataset.id;
-
-//     if (e.target.classList.contains("delete-btn")){
-//       deleteStudent(id);}
-
-
-
-// if (e.target.classList.contains("edit-btn")) {
-//       updateStudent(id); 
-//     }
-
-//   })
-// })
-
-
-
-
-
-// // Функція для отримання всіх студентів
-
-// function getStudents() {
-
-
-// fetch("http://localhost:3000/students")
-// .then(response => response.json())
-// .then(data => {
-// renderStudents(data);
-// })
-// .catch(error => console.log(error));
-// }
-
-
-
-
-
-// // Функція для відображення студентів у таблиці
-
-// function renderStudents(students) {
-
-// const tbody = document.querySelector("#students-table tbody");
-// tbody.innerHTML = "";
-// students.forEach(student => {
-//   const row = document.createElement('tr');
-//   row.innerHTML = `
-//   <td>${student.id}</td>
-// <td>${student.name}</td>
-// <td>${student.age}</td>
-// <td>${student.course}</td>
-// <td>${student.skills.join(", ")}</td>
-// <td>${student.email}</td>
-// <td>${student.isEnrolled ? 'Так' : 'Ні'}</td>
-//       <td>
-//         <button class="edit-btn" data-id="${student.id}">Оновити</button>
-//         <button class="delete-btn" data-id="${student.id}">Видалити</button>
-//       </td>`;
-//       tbody.appendChild(row)
-// })
-
- 
-
-// }
-
-
-
-
-
-
-// function addStudent(e) {
-//   e.preventDefault(); 
-  
-//   const nameValue = document.getElementById("name").value;
-//   const ageValue = Number(document.getElementById("age").value);
-//   const courseValue = document.getElementById('course').value;
-//   const emailValue = document.getElementById('email').value;
-//   const isEnrolledValue = document.getElementById('isEnrolled').checked;
-//   const skillsValue = document.getElementById('skills').value.split(',').map(s => s.trim());
-
-
-//   const newStudent = {
-//     name: nameValue,
-//     age: ageValue,
-//     course: courseValue,
-//     skills: skillsValue,
-//     email: emailValue,
-//     isEnrolled: isEnrolledValue
-//   };
-
-//   fetch("http://localhost:3000/students", {
-//     method: "POST", 
-//     headers: {
-//       "Content-Type": "application/json" 
-//     },
-//     body: JSON.stringify(newStudent) 
-//   })
-//   .then(response => response.json())
-//   .then(() => {
-    
-//     document.getElementById('add-student-form').reset();
-    
-   
-//     getStudents();
-//   })
-//   .catch(error => console.log(error));
-// }
-
-
-
-// // Функція для оновлення студента
-
-// function updateStudent(id) {
-//   const newCourse = prompt("Введіть новий курс для студента");
-
-
-//   const updatedData = {
-//     course: newCourse
-//   };
-
-//   fetch(`http://localhost:3000/students/${id}`, {
-//     method: "PATCH", 
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify(updatedData)
-//   })
-//   .then(() => {
-//     getStudents(); 
-//   })
-//   .catch(error => console.log(error));
-// } 
-
-
-
-
-// // Функція для видалення студента
-
-// function deleteStudent(id) {
-// fetch(`http://localhost:3000/students/${id}`, {
-//   method: "DELETE"
-// })
-
-
-// .then(() => {
-//     getStudents(); 
-//   })
-
-// }
-
-
-
-
-
-
 
 
 
@@ -247,26 +91,45 @@ function addStudent(e) {
 }
 
 // Функція для оновлення студента (PATCH)
-function updateStudent(id) {
-  const newCourse = prompt("Введіть новий курс для студента");
-  if (!newCourse) return; // Якщо користувач скасував або нічого не ввів
+function updateStudent(id, data) {
+  const options = {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    };
 
-  const updatedData = {
-    course: newCourse
-  };
+//   fetch(`http://localhost:3000/students/${id}`, {
+//     method: "PATCH", 
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(updatedData)
+//   })
+//   .then(() => {
+//     getStudents(); 
+//   })
+//   .catch(error => console.log(error));
+// } 
 
-  fetch(`http://localhost:3000/students/${id}`, {
-    method: "PATCH", 
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(updatedData)
-  })
-  .then(() => {
-    getStudents(); 
-  })
-  .catch(error => console.log(error));
-} 
+
+return fetch(`http://localhost:3000/students/${id}`, options)
+        .then(res => {
+          
+        
+           
+            return res.json();
+        })
+        .catch(error => {
+            console.error("помилка в updateStudent:", error);
+            throw error;
+        });
+
+
+
+      }
+
 
 // Функція для видалення студента (DELETE)
 function deleteStudent(id) {
